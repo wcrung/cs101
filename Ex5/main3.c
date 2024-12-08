150b9e48d7e15566324f634eac6e7fb687641cab
wcrung <crwang1006@gmail.com>
2024-12-08 22:24:01 +0800

    Update main3.c

--- Ex5/main3.c
+++ Ex5/main3.c
@@ -1,1 +1,14 @@
-
+#include <stdio.h>
+int main()
+{
+    for (int i = 1; i <= 81; i++) {
+        int y = (i-1) / 9 + 1; 
+        int x = (i-1) % 9 + 1; 
+        printf("%d*%d=%d\t", y, x, y*x);
+        
+        if (x == 9) {
+            printf("\n");
+        }
+    }
+    return 0;
+}
