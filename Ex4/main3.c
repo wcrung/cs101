c3f7311b884e988e7d689d26b09e615d3d9e7d6a
wcrung <crwang1006@gmail.com>
2024-12-12 15:29:45 +0800

    Update main3.c

--- Ex4/main3.c
+++ Ex4/main3.c
@@ -3,7 +3,11 @@ int main()
 {
     for (int i=1, j=0; i<=9;){
         printf("%d*%d=%d\t", i, j, i*++j);
-        (j == 9)?(i++, j=0, printf("\n")):i;
+        if(j==9){
+            i++;
+            j=0;
+            printf("\n");
+        }
     }
     return 0;
 }
