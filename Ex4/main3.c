c77b72be39b39e20903d0f6a10e19fdbc27f230f
wcrung <crwang1006@gmail.com>
2024-12-12 15:09:51 +0800

    Update main3.c

--- Ex4/main3.c
+++ Ex4/main3.c
@@ -1,14 +1,9 @@
 #include <stdio.h>
 int main()
 {
-    for (int i = 1; i <= 81; i++) {
-        int y = (i-1) / 9 + 1; 
-        int x = (i-1) % 9 + 1; 
-        printf("%d*%d=%d\t", y, x, y*x);
-        
-        if (x == 9) {
-            printf("\n");
-        }
+    for (int i=1, j=0; i<=9;){
+        printf("%d*%d=%d\t", i, j, i*++j);
+        (j == 9)?(i++, j=0, printf("\n")):i;
     }
     return 0;
 }
