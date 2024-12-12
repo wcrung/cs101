69b69097129694d419c28218265eb380805addaf
wcrung <crwang1006@gmail.com>
2024-12-12 14:49:24 +0800

    Update main1.c

--- Ex4/main1.c
+++ Ex4/main1.c
@@ -1,19 +1,16 @@
 #include <stdio.h>
 int main()
 {
-    int n=7;
-    int spc=n;
-    for (int i=1; i<=n; i++){
-        for (int k=1; k<=spc+i; k++){
-            if (k<=spc){
-                printf(" ");
-            }
-            else{
-                printf("%d ", i);
-            }
+    int n = 7;
+    int i, j;
+    for (i =1; i <= n; i++){
+        for (j = 1; j <= n-i; j++){
+            printf(" ");
+        }
+        for(j = 1; j <= i; j++){
+            printf("%d ", i);
         }
         printf("\n");
-        spc--;
     }
     return 0;
 }
